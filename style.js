c3d5a322e3d2526f71b5e65be2d0048af69a462d
JagathRaveendran <147247110+JagathRaveendran@users.noreply.github.com>
2023-12-02 16:59:40 +0530

    cal

---
+++ style.js
@@ -0,0 +1,145 @@
+ let totalIncome = 0;
+let totalExpence = 0;
+
+
+function btnsubmit() {
+  let expenseStatus1 = false;
+  let expenseStatus2 = false;
+  let status1 = false;
+
+  const catogoryOUT = document.getElementById("catogoryexpence").value;
+  const Expense = parseFloat(document.getElementById("Expense").value);
+  let error;
+
+  if (catogoryOUT == '') {
+    error = "not Valid";
+    document.getElementById("e-cat-OUT").style.color = "red";
+  } else {
+    error = "valid";
+    document.getElementById("e-cat-OUT").style.color = "green";
+    expenseStatus1 = true;
+  }
+
+  document.getElementById("e-cat-OUT").innerHTML = error;
+
+  if (isNaN(Expense) || Expense <= 0) {
+    error = "not valid";
+    document.getElementById("e-money-OUT").style.color = "red";
+  } else {
+    error = "valid";
+    document.getElementById("e-money-OUT").style.color = "green";
+    expenseStatus2 = true;
+  }
+
+  document.getElementById("e-money-OUT").innerHTML = error;
+
+  if (expenseStatus1 && expenseStatus2) {
+    status1 = true;
+    const tb = document.getElementById("tbe");
+    const tr = tb.insertRow();
+    const cat = tr.insertCell(0);
+    const ine = tr.insertCell(1);
+
+    cat.innerHTML = catogoryOUT;
+    ine.innerHTML = Expense;
+    document.getElementById("catogoryexpence").value = "";
+    document.getElementById("Expense").value = "";
+  }
+  if (status1) {
+    totalExpence = Expense + totalExpence;
+    document.getElementById("totalEx").innerHTML = totalExpence;
+    chart();
+  }
+}
+
+function btnsubmit1() {
+  let incomeStatus1 = false;
+  let incomeStatus2 = false;
+  let status = false;
+
+  const catogoryIN = document.getElementById("catogoryincome").value;
+  const Income = parseFloat(document.getElementById("Income").value);
+  let error;
+
+  if (catogoryIN == '') {
+    error = "not Valid";
+    document.getElementById("e-cat-IN").style.color = "red";
+  } else {
+    error = "valid";
+    document.getElementById("e-cat-IN").style.color = "green";
+    incomeStatus1 = true;
+  }
+
+  document.getElementById("e-cat-IN").innerHTML = error;
+
+  if (isNaN(Income) || Income <= 0) {
+    // Check if Income is not a valid number or is less than or equal to 0
+    error = "not valid";
+    document.getElementById("e-money-IN").style.color = "red";
+  } else {
+    error = "valid";
+    document.getElementById("e-money-IN").style.color = "green";
+    incomeStatus2 = true;
+  }
+
+  document.getElementById("e-money-IN").innerHTML = error;
+
+  if (incomeStatus1 && incomeStatus2) {
+    status = true;
+    const tb = document.getElementById("tbi");
+    const tr = tb.insertRow();
+    const cat = tr.insertCell(0);
+    const ine = tr.insertCell(1);
+
+    cat.innerHTML = catogoryIN;
+    ine.innerHTML = Income;
+
+    document.getElementById("catogoryincome").value = "";
+    document.getElementById("Income").value = "";
+  }
+  if (status) {
+    totalIncome = Income + totalIncome;
+    document.getElementById("totalIn").innerHTML = totalIncome;
+    chart();
+  }
+}
+
+function chart() 
+{
+  var xValues = ["income", "Expense", "Balance"];
+  var yValues = [totalIncome, totalExpence, totalIncome - totalExpence];
+  var barColors = ["#00aba9", "#b91d47", "#2b5797"];
+  const Balence = totalIncome - totalExpence;
+  document.getElementById("Balence").innerHTML = Balence;
+  if( totalExpence > totalIncome )
+  {
+    document.getElementById("score").style.color ="red";;
+    document.getElementById("score").innerHTML = "Your Expense is too heigh ";
+  }
+  else
+  {
+    document.getElementById("score").style.color ="Green";;
+    document.getElementById("score").innerHTML = "good job ";
+  }
+  if( Balence < 0)
+  {
+    alert("Balence is minus");
+  }
+  new Chart("myChart", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Balance chart"
+      }
+    }
+  });
+  
+}
